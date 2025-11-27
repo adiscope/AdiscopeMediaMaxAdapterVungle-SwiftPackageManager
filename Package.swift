ce6fbea4e9b15dccef3d252605e7b0f3346b218c
kyungboshim <kyungbo.shim@neowiz.com>
2025-11-27 15:48:35 +0900

    5.0.0 배포

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.1.0"),
-        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.4.4"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.4.0"),
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.5.2"),
     ],
     targets: [
         .target(
             name: "AdiscopeMediaMaxAdapterVungleTarget",
             dependencies: [
                 .target(name: "AdiscopeMediaMaxAdapterVungle"),
+                .target(name: "AdiscopeMediaMaxAdapterVungleMediationAdapter"),
                 .product(name: "VungleAdsSDK", package: "VungleAdsSDK-SwiftPackageManager"),
                 .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
             ],
@@ -28,8 +29,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AdiscopeMediaMaxAdapterVungle",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.4.1/AdiscopeMediaMaxAdapterVungle.zip",
-            checksum: "282c481ed54590f48cd4869b5508fbb0a94afab2a91725e2950ea3ee220e7058"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/AdiscopeMediaMaxAdapterVungle.zip",
+            checksum: "c64dd3e9e70051b0e62d162907cf38beee4c25762c16a0daed366961b300a492"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterVungleMediationAdapter",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/AppLovinMediationVungleAdapter.xcframework.zip",
+            checksum: "db857f31a04693033c61d8e202b62c5f3c7a0976e9a1f24b45053820e974f532"
         ),
     ]
 )
